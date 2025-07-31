2c7f3dd7d20a2395d3e82ae4958bff4227318e6c
Syed Sahil <syedsahil28803@gmail.com>
2025-07-31 19:45:49 +0530

    Add new function to utilities and rectify river version

--- utilities.py
+++ utilities.py
@@ -5,6 +5,9 @@ pd.set_option('display.float_format', '{:.2f}'.format)
 pd.set_option("display.width", 200)  # Set large enough to avoid wrapping
 pd.set_option("display.max_columns", None)  # Show all columns
 
+def return_file_paths(path):
+    return sorted(glob(path))
+
 class Formatter:
     def return_formatted_string(self, string, symbol = '-'):
         decrator = symbol * ((200 - len(string))//2 + 1) if len(string)&1 else symbol * ((200 - len(string))//2)
